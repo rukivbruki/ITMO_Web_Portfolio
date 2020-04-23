213be97c96ecf3dc3926a8f60e869a91bc158be9
0994486@gmail.com <6234Ai1949sa>
2020-04-23 08:18:59 +0300

    Добавляет Crypto

---
+++ Crypto/publicDecrypt.js
@@ -0,0 +1,11 @@
+/*isaev*/
+
+const crypto = require('crypto')
+const fs = require('fs')
+
+const url = crypto.publicDecrypt(
+  fs.readFileSync('./Crypto/key'),
+    fs.readFileSync('./Crypto/secret')
+)
+
+console.log(url.toString('utf8'))
---
+++ Crypto/publicEncrypt.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
+
+let myDecrypt = fs.readFileSync(path.join(__dirname, './publicDecrypt.js'))
+let key = fs.readFileSync(path.join(__dirname, 'key'))
+
+fs.writeFileSync(
+  path.join(__dirname, 'message'),
+  crypto.publicEncrypt(key.toString('utf8'), myDecrypt)
+)
+
+
